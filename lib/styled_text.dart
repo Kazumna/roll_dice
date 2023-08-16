9329ee560508d46471a04209cdd50c1a95d6f6d8
Ady <ady@dnsspecialistservices.com.au>
2023-08-17 09:42:27 +1000

    step by step usage of passing value and using final

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -2,13 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  String text;
+  //Store it      //Adding final key word mean it will never change internally
+  final String text;
 
-  StyledText(this.text, {super.key});
+  //Accept text as an argument
+  const StyledText(this.text, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Text(
+      //use it
       text,
       style: const TextStyle(
         color: Colors.white,

745d619bb309141e134732673f9269f04f9fd1e6
Ady <ady@dnsspecialistservices.com.au>
2023-08-17 10:49:40 +1000

    added images

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice/styled_text.dart';
 
 //final mean this data container variable will never receive a new value
 // final startAlignment = Alignment.topLeft;
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
-
 class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
-  const GradientContainer(this.color1, this.color2,{super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  ///Class can have multiple constructors
+  // GradientContainer.purple({super.key})
+  //     : color1 = Colors.deepPurple,
+  //       color2 = Colors.indigo;
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +26,12 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello World!'),
+      child: Center(
+        child: Image.asset(
+          "assets/images/dice-2.png",
+          width: 200,
+        ),
       ),
     );
   }
-
 }
